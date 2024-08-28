a557b6051ff36f6230c40bd48add98f01de11140
skvuemotion <skumar@vuemotionteam.com>
2024-08-28 22:52:33 +0530

    update

--- books.py
+++ books.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from fastapi import FastAPI
 from enum import Enum
 
@@ -10,25 +11,14 @@ BOOKS = {
     'book_4' : {'title' : 'Title Four' , 'author' : 'Author Four'},
     'book_5' : {'title' : 'Title Five' , 'suthor' : 'Author Five'},
 }
-
-class DirectionName(str, Enum):
-    north = "North"
-    south = "South"
-    east = "East"
-    west = "West"
-
-
-
 @app.get("/")
-async def read_all_books():
+async def read_all_books(skip_book: Optional[str] = None):
+    if skip_book:
+        new_books = BOOKS.copy()
+        del new_books[skip_book]
+        return new_books
     return BOOKS
 
-@app.get("/direction/{direction_name}")
-async def get_direction(direction_name: DirectionName):
-    if direction_name == DirectionName.north:
-        return{"Direction": direction_name, "sub": "UP" }
-    if direction_name == DirectionName.south:
-        return{"Direction": direction_name, "sub": "DOWN"}
-    if direction_name == DirectionName.west:
-        return{"Direction": direction_name, "sub": "LEFT" }
-    return{"Direction": direction_name, "sub": "RIGHT"}
+@app.get("/{book_name}")
+async def read_book(book_name: str):
+    return BOOKS[book_name]
